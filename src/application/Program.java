5e1ec048435980bb2068347839cd10cea7591365
rtrnonato <rtrnonato@gmail.com>
2023-11-20 20:44:22 -0300

    Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.printMatch(chessMatch, captured);
                 System.out.println();
@@ -44,6 +44,6 @@ public class Program {
                 sc.nextLine();
             }
         }
-
+        UI.printMatch(chessMatch, captured);
     }
 }
